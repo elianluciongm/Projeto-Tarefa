f627190175ace9125855018e82dab752ab44c23e
elianluciongm <elianluciongm@gmail.com>
2026-07-03 19:02:51 -0300

    Adicionar SSL na conexão do banco

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,6 +7,15 @@ if (process.env.DATABASE_URL) {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: "postgres",
         logging: false,
+        dialectOptions:
+            process.env.DB_SSL === "true"
+                ? {
+                    ssl: {
+                        require: true,
+                        rejectUnauthorized: false,
+                    },
+                }
+                : {},
     });
 } else {
     sequelize = new Sequelize(
